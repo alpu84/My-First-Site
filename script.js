8c59ce87c4167084b0cc4bd507d1683ac8c14113
alpu84 <alpu84@gmail.com>
2021-04-04 12:00:43 -0600

    First Commit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.onload = function() {
     var button = document.querySelector('button');
   
     button.addEventListener('click', function() {
-      alert("Let's Rock the World on Sunday!!");
+      alert("Let's Rock the World on Sunday 2021!!");
     });
   }
